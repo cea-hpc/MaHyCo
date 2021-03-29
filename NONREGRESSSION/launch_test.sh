bb1a30bb92bd8e1c0c6bd8071a69f6fd67f8b613
PERLAT JEAN-PHILIPPE <jean-philippe.perlat@wanadoo.fr>
2021-03-29 16:39:10 +0200

    ajout de la nonreg

---
+++ NONREGRESSSION/launch_test.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# Launch the test and compares obtained results to expected ones
+set -euo pipefail 
+
+# This function launch the computation by calling the executable with arguments
+# taken from args.txt file
+function launch_computation {
+  local readonly exe_path=$1
+  local readonly data_dir=$2
+  local return_code=0
+  $1 $data_dir/Donnees.arc
+  if [[ $? -ne 0 ]]; then
+    echo "A problem occured during test execution."
+    return_code=1
+  fi
+  return ${return_code}
+}
+
+# This function compares the results obtained in the output directory with those
+# expected in the reference directory
+function compare_results {
+  local readonly reference_dir=$1
+  local return_code=0
+  ls -l "$reference_dir/output"
+  if ! diff -r output/depouillement "$reference_dir/output/depouillement" > /dev/null 2>&1; then
+      echo "Test failure! Output is different from reference"
+      echo ${test_dir} >>  list_of_cases_to_change
+    return_code=1
+  fi
+  return ${return_code}
+}
+
+# Main function. Calls launch_computation and compare_results
+# For each test a directory is created inside /tmp
+function main {
+  local readonly exe_path=$1
+  local readonly test_dir=$2
+
+  echo "Executable path : ${exe_path}"
+  echo "Executing test $(basename ${test_dir})"
+
+  tmp_dir=$(mktemp -d -t mahyco-ci-XXXXXXXXXX)
+
+  if [[ ! -d ${tmp_dir} ]]; then
+    echo "Unable to create a temporary directory!"
+    echo "Aborting!"
+    exit 3
+  fi
+
+  cd ${tmp_dir}
+  echo "This directory contains the output of the test under ${test_dir}" > README.txt
+  pwd
+  launch_computation ${exe_path} ${test_dir}
+  if [[ $? -ne 0 ]]; then
+    echo "Aborting!"
+    exit 1
+  fi
+
+  compare_results ${test_dir}
+  if [[ $? -ne 0 ]]; then
+    echo "Aborting!"
+    exit 2
+  else
+    echo "Success!"
+  fi
+  exit 0
+}
+
+main $@
