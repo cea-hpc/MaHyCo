9700698d439dd82c5aa86c52bce11a95aae9654d
meltzb <bertrand.meltz@protonmail.com>
2021-12-02 15:09:49 +0100

    Suppression vars cmake inutile

--- ArcaneCompilerConfig.cmake
+++ ArcaneCompilerConfig.cmake
@@ -1,8 +1,6 @@
 # Indique qu'on veut des bibliothèques dynamiques (indispensable avec les services)
 set(BUILD_SHARED_LIBS TRUE)
 set(CMAKE_VERBOSE_MAKEFILE TRUE CACHE BOOL "Verbose makefile?" FORCE)
-set(CMAKE_C_COMPILER "/usr/bin/cc" CACHE PATH "C Compiler" FORCE)
-set(CMAKE_CXX_COMPILER "/usr/bin/c++" CACHE PATH "C++ Compiler" FORCE)
 set(CMAKE_CXX_FLAGS_INIT "  " CACHE STRING "Default flags for C++ compiler" FORCE)
 set(CMAKE_EXE_LINKER_FLAGS "" CACHE STRING "Default flags for linker" FORCE)
 set(CMAKE_SHARED_LINKER_FLAGS "" CACHE STRING "Default flags for linker" FORCE)
