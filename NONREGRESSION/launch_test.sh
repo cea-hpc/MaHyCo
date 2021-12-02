bb9cdcbb21da5b8fc5b85ee5f8e630d3b5b2edfc
meltzb <bertrand.meltz@protonmail.com>
2021-12-02 15:59:55 +0100

    Nettoyage non regression pour se conformer à la branche Mahyco_gpu2

--- NONREGRESSION/launch_test.sh
+++ NONREGRESSION/launch_test.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Launch the test and compares obtained results to expected ones
 set -euo pipefail 
+#set -x 
 
 # This function launch the computation by calling the executable with arguments
 # taken from args.txt file
 function launch_computation {
   local readonly exe_path=$1
   local readonly data_dir=$2
+  local readonly mpi_launcher=$3
   local return_code=0
-  $1 $data_dir/Donnees.arc
+  ${mpi_launcher} -n 1 $1 $data_dir/Donnees.arc
   if [[ $? -ne 0 ]]; then
     echo "A problem occured during test execution."
     return_code=1
@@ -20,9 +22,10 @@ function launch_computation {
 function launch_computation_seq_pr {
   local readonly exe_path=$1
   local readonly data_dir=$2
+  local readonly mpi_launcher=$3
   local return_code=0
-  $1 -arcane_opt max_iteration 10 $data_dir/Donnees.arc
-  $1 -arcane_opt continue $data_dir/Donnees.arc
+  ${mpi_launcher} -n 1 $1 -arcane_opt max_iteration 10 $data_dir/Donnees.arc
+  ${mpi_launcher} -n 1 $1 -arcane_opt continue $data_dir/Donnees.arc
   if [[ $? -ne 0 ]]; then
     echo "A problem occured during test execution."
     return_code=1
@@ -34,8 +37,9 @@ function launch_computation_seq_pr {
 function launch_computation_para_4 {
   local readonly exe_path=$1
   local readonly data_dir=$2
+  local readonly mpi_launcher=$3
   local return_code=0
-  mpiexec -n 4 $1 $data_dir/Donnees.arc
+  ${mpi_launcher} -n 4 $1 $data_dir/Donnees.arc
   if [[ $? -ne 0 ]]; then
     echo "A problem occured during test execution."
     return_code=1
@@ -47,8 +51,9 @@ function launch_computation_para_4 {
 function launch_computation_para_8 {
   local readonly exe_path=$1
   local readonly data_dir=$2
+  local readonly mpi_launcher=$3
   local return_code=0
-  mpiexec -n 8 $1 $data_dir/Donnees.arc
+  ${mpi_launcher} -n 8 $1 $data_dir/Donnees.arc
   if [[ $? -ne 0 ]]; then
     echo "A problem occured during test execution."
     return_code=1
@@ -61,10 +66,11 @@ function compare_results {
   local readonly reference_dir=$1
   local return_code=0
   ls -l "$reference_dir/output"
-  if ! diff -r output/depouillement "$reference_dir/output/depouillement" > /dev/null 2>&1; then
+  echo "Differences in : ${PWD}/DIFF.txt"
+  diff -r output/depouillement "$reference_dir/output/depouillement" > DIFF.txt 2>&1
+  if [[ $? -ne 0 ]]; then
       echo "Test failure! Output is different from reference"
       echo ${test_dir} >>  list_of_cases_to_change
-      diff -r output/depouillement "$reference_dir/output/depouillement" > DIFF.txt
     return_code=1
   fi
   return ${return_code}
@@ -76,15 +82,26 @@ function main {
   local readonly exe_path=$1
   local readonly test_dir=$2
   local readonly type=$3
+  local readonly mpi_launcher=$4
+  local readonly basetmpdir_nr=$5
 
+  local readonly test_name=$(basename ${test_dir})
   echo "Executable path : ${exe_path}"
-  echo "Executing test $(basename ${test_dir})"
+  echo "Executing test ${test_name}"
   echo "Executing test mode ${type}"
   if [ ${type} = "para" ]; then
     echo "Executing test parallele mode ${para}"
   fi
   
-  tmp_dir=$(mktemp -d -t mahyco-ci-XXXXXXXXXX)
+  tmpdir_base="${basetmpdir_nr}/MAHYCO/NONREGRESSION"
+  mkdir -p ${tmpdir_base}
+  if [ ${?} != 0 ]
+  then
+    echo "Unable to create ${$tmpdir_base}"
+    echo "Aborting!"
+    exit 4
+  fi
+  tmp_dir=$(mktemp -d -p ${tmpdir_base} mahyco-ci-${type}-${test_name}-XXXXXXXXXX)
 
   if [[ ! -d ${tmp_dir} ]]; then
     echo "Unable to create a temporary directory!"
@@ -99,18 +116,18 @@ function main {
   if [ ${type}  = "para_8" ]
   then
       echo " lancement parallele sur 8 coeurs" 
-      launch_computation_para_8 ${exe_path} ${test_dir}
+      launch_computation_para_8 ${exe_path} ${test_dir} ${mpi_launcher}
   elif [ ${type}  = "para_4" ]
   then
       echo " lancement parallele sur 4 coeurs" 
-      launch_computation_para_4 ${exe_path} ${test_dir}
+      launch_computation_para_4 ${exe_path} ${test_dir} ${mpi_launcher}
   elif [ ${type}  = "seq_pr" ]
   then
       echo " lancement sequentiel protection-reprise" 
-      launch_computation_seq_pr ${exe_path} ${test_dir}    
+      launch_computation_seq_pr ${exe_path} ${test_dir} ${mpi_launcher}   
   else
       echo " lancement sequentiel" 
-      launch_computation ${exe_path} ${test_dir}
+      launch_computation ${exe_path} ${test_dir} ${mpi_launcher}
   fi    
   if [[ $? -ne 0 ]]; then
     echo "Aborting!"
