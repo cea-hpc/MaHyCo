1a59564813d4327d64f7af734480fdd722ef253c
perlatj <jean-philippe.perlat@cea.fr>
2023-02-21 15:22:32 +0100

    Rangement des directives pour compiler avec cmake

---
+++ memo_cmake.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+rm -rf build
+mkdir build
+cd build
+
+#tentative pour compiler en full nvhpc mais on link qd meme avec cc
+#NVCC_BIN=/opt/nvidia/hpc_sdk/Linux_x86_64/21.2/compilers/bin/
+#CXX=${NVCC_BIN}/nvc++
+#CC=${NVCC_BIN}/nvcc
+#export CXX CC
+
+# Build with makefiles in parallel
+cmake .. -DCMAKE_BUILD_TYPE=Release -DArcane_ROOT="/home/perlatj/Workspace/arcane/"
+#cmake .. -DCMAKE_BUILD_TYPE=Release -DArcane_ROOT="/home/perlatj/Workspace/arcane_cuda/"
+cmake --build . -- -j16 
+
+# Build with Ninja (natively parallel)
+#cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DArcane_ROOT="/home/meltzb/workspace/arcane/" -DWANT_CUDA=TRUE -DWANT_PROF_ACC=TRUE
+
+#cmake --build . --target test
+
+cd /home/perlatj/Workspace/MaHyCo/build/src
+cp ../../NONREGRESSION/CAS_BiSodCaseX/Donnees.arc .
+mpiexec -n 4 ./Mahyco Donnees.arc
