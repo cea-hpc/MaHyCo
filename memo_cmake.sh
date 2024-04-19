b9a73cee46885114d6d3df5187132aec9b2215b6
Jean-Philippe Perlat <jean-philippe.perlat@cea.fr>
2024-04-19 11:47:10 +0200

    Modification de ArcaneCompilerConfig.cmake, CMakeLists.txt, memo_cmake.sh pour INTI

--- memo_cmake.sh
+++ memo_cmake.sh
@@ -1,27 +1,42 @@
 #!/bin/bash
 
 
-rm -rf build
-mkdir build
-cd build
 
 #tentative pour compiler en full nvhpc mais on link qd meme avec cc
 #NVCC_BIN=/opt/nvidia/hpc_sdk/Linux_x86_64/21.2/compilers/bin/
 #CXX=${NVCC_BIN}/nvc++
 #CC=${NVCC_BIN}/nvcc
 #export CXX CC
+host=`hostname -d`
+if [ "$host" = "c-inti.mg1.ccc.ocre.cea.fr" ]; then
+    echo "Passage pour INTI"
+    rm -r $OWN_CCCWORKDIR/MaHyCo/build
+    mkdir $OWN_CCCWORKDIR/MaHyCo/build
+    cd $OWN_CCCWORKDIR/MaHyCo/build
+    cmake $HOME/workspace/MaHyCo -DCMAKE_BUILD_TYPE=Release -DArcane_ROOT="$HOME/local_arcane" -DCINETIQUE_SRC="$HOME/workspace//Cinetique_chgt_phase/"
 
-# Build with makefiles in parallel
-cmake .. -DCMAKE_BUILD_TYPE=Release -DArcane_ROOT="/home/perlatj/Workspace/arcane/" -DCINETIQUE_SRC="/home/perlatj/Workspace/Cinetique_chgt_phase/"
-#cmake .. -DCMAKE_BUILD_TYPE=Release -DArcane_ROOT="/home/perlatj/Workspace/arcane/"
-#cmake .. -DCMAKE_BUILD_TYPE=Release -DArcane_ROOT="/home/perlatj/Workspace/arcane_cuda/"
-cmake --build . -- -j16 
+    cmake --build . -- -j16 
+    cd $OWN_CCCWORKDIR/MaHyCo/build/src
+    
+    cp $HOME/workspace/MaHyCo/NONREGRESSION/CAS_BiSodCaseX/Donnees.arc .
+    mpiexec -n 1 ./Mahyco Donnees.arc
+    rm -rf Donnees.arc output
+else
+    echo "Passage pour Udbuntu"
+    rm -rf build
+    mkdir build
+    cd build
+    cmake .. -DCMAKE_BUILD_TYPE=Release -DArcane_ROOT="$HOME/arcane/" -DCINETIQUE_SRC="$HOME/Cinetique_chgt_phase/"
+    #cmake .. -DCMAKE_BUILD_TYPE=Release -DArcane_ROOT="$HOME/Workspace/arcane/"
+    #cmake .. -DCMAKE_BUILD_TYPE=Release -DArcane_ROOT="$HOME/Workspace/arcane_cuda/"
+    cmake --build . -- -j16 
 
-# Build with Ninja (natively parallel)
-#cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DArcane_ROOT="/home/meltzb/workspace/arcane/" -DWANT_CUDA=TRUE -DWANT_PROF_ACC=TRUE
+    # Build with Ninja (natively parallel)
+    #cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DArcane_ROOT="$HOME/workspace/arcane/" -DWANT_CUDA=TRUE -DWANT_PROF_ACC=TRUE
 
-#cmake --build . --target test
+    #cmake --build . --target test
+    cp ../../NONREGRESSION/CAS_BiSodCaseX/Donnees.arc .
+    mpiexec -n 1 ./Mahyco Donnees.arc
+    rm -rf Donnees.arc output
+fi
 
-cd /home/perlatj/Workspace/MaHyCo/build/src
-cp ../../NONREGRESSION/CAS_BiSodCaseX/Donnees.arc .
-mpiexec -n 1 ./Mahyco Donnees.arc
