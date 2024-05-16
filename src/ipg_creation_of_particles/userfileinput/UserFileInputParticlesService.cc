0d6748a990dffc05262d0a0b05458e09f7ff7584
Lucas Prouvost <lucas.prouvost@cea.fr>
2024-05-16 11:55:37 +0200

    version 0 (edtion des liens et creation du service "vide") du service de creation de particules à partir d'un fichier utilisateur

---
+++ src/ipg_creation_of_particles/userfileinput/UserFileInputParticlesService.cc
@@ -0,0 +1,33 @@
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+#include "UserFileInputParticlesService.h"
+
+using namespace Arcane;
+
+/*---------------------------------------------------------------------------*/
+/* particule creation in compute-loop */
+/*---------------------------------------------------------------------------*/
+
+void UserFileInputParticlesService::createParticles(IParticleFamily* m_particles_family)
+{
+  // we fill in the list of particule injected in the simulation
+
+
+
+  // we fill out the list of particules to be injected
+
+}
+
+/*---------------------------------------------------------------------------*/
+/* initialisation in start-init: read the user file and store particule data */
+/*---------------------------------------------------------------------------*/
+
+void UserFileInputParticlesService::initParticles()
+{
+  // we fill in the familly of particules which will later be introduce in the simulation
+
+}
+
+/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+ARCANE_REGISTER_SERVICE_USERFILEINPUTPARTICLES(UserFileInputParticles, UserFileInputParticlesService);
---
+++ src/ipg_creation_of_particles/userfileinput/UserFileInputParticlesService.h
@@ -0,0 +1,64 @@
+﻿// -*- tab-width: 2; indent-tabs-mode: nil; coding: utf-8-with-signature -*-
+#ifndef USERFILEINPUTPARTICULESSERVICE_H
+#define USERFILEINPUTPARTICULESSERVICE_H
+
+
+#include "arcane/IParticleFamily.h"
+
+#include "ipg_creation_of_particles/ICreationOfParticles.h"
+#include "ipg_creation_of_particles/userfileinput/UserFileInputParticles_axl.h"
+
+
+
+using namespace Arcane;
+
+
+/**
+Interface de la creation de particules.
+
+*/
+
+
+class UserFileInputParticlesService
+: public ArcaneUserFileInputParticlesObject
+{
+public:
+  /** Constructeur de la classe */
+  UserFileInputParticlesService(const ServiceBuildInfo & sbi) 
+    : ArcaneUserFileInputParticlesObject(sbi) {}
+  /** Destructeur de la classe */
+  virtual ~UserFileInputParticlesService() {};
+  
+  /** 
+   *  Crée les particules et leurs propriétés (poids, position, vitesse, 
+   *  rayon, température) à partir des données stockées lors de initParticles.
+   */
+  virtual void createParticles(IParticleFamily* m_particles_family);
+
+  /** 
+   * Etapes à faire lors de start-init : lecture du fichier utilisateur
+   * et stockage des données.
+   */
+  virtual void initParticles();
+
+
+
+// private:
+//   m_particles_not_yet_created_family;
+  
+};
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
