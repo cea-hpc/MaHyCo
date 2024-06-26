cd10eeee07f44f0f656324664902f4849400b4ff
Marie Spraul <marie.spraul@cea.fr>
2024-06-26 11:24:12 +0200

    Ajoute un script python pour tracer une grandeur timehistory pour un milieu donné

---
+++ utils/plot_time_history.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env/python3
+
+# Script pout tracer les sorties timehistory avec matplotlib
+
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+import pathlib
+
+
+parser = argparse.ArgumentParser("Plot the TimeHistory file in argument")
+parser.add_argument("filepath", type=str, help="Path to the file to plot")
+args = parser.parse_args()
+
+title = pathlib.PurePath(args.filepath).name
+var_name = title.split(sep="_")[0]
+table = np.loadtxt(args.filepath)
+plt.figure()
+plt.plot(table[:, 0], table[:, 1])
+plt.xlabel("Time [s]")
+plt.ylabel(var_name)
+plt.title(title)
+plt.show()
