99edb4bf7c4bc977c6aee818a333a47f0edb49e3
Marie Spraul <marie.spraul@cea.fr>
2024-06-26 11:35:01 +0200

    Prend en compte le tracé des sorties Arcane GlobalTime, ... dont le nom n'est pas formatté dans Mahyco

--- utils/plot_time_history.py
+++ utils/plot_time_history.py
@@ -13,7 +13,8 @@ parser.add_argument("filepath", type=str, help="Path to the file to plot")
 args = parser.parse_args()
 
 title = pathlib.PurePath(args.filepath).name
-var_name = title.split(sep="_")[0]
+var_name = title.split(sep="_")[0] if "Time" not in title else title
+
 table = np.loadtxt(args.filepath)
 plt.figure()
 plt.plot(table[:, 0], table[:, 1])
