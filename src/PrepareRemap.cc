aee6f8d2626f6429a9a2059cf5cf6013da434cac
Marie Spraul <marie.spraul@cea.fr>
2024-07-31 17:35:30 +0200

    Suppression de GlobalTime dans les sorties TimeHistory

--- src/PrepareRemap.cc
+++ src/PrepareRemap.cc
@@ -238,15 +238,13 @@ void MahycoModule::remap() {
           // m_internal_energy_n[ev] = m_internal_energy[ev];
         }
     }
-    if (!options()->sansLagrange) {
-        for (Integer index_env=0; index_env < m_nb_env ; index_env++) {
-        IMeshEnvironment* ienv = mm->environments()[index_env];
-        // Calcul de la pression et de la vitesse du son
-        options()->environment[index_env].eosModel()->applyEOS(ienv);
-        // Test endommagement --> Pression devient nulle ?
-        options()->environment[index_env].eosModel()->Endommagement(ienv);
-      }
-      computePressionMoyenne();
-   }
+    for (Integer index_env=0; index_env < m_nb_env ; index_env++) {
+      IMeshEnvironment* ienv = mm->environments()[index_env];
+      // Calcul de la pression et de la vitesse du son
+      options()->environment[index_env].eosModel()->applyEOS(ienv);
+      // Test endommagement --> Pression devient nulle ?
+      options()->environment[index_env].eosModel()->Endommagement(ienv);
+    }
+    computePressionMoyenne();
   }
 }
