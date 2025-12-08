1c50a218eebbc774ba3a5a8be84cdf01656c338b
Bertrand Meltz <bertrand.meltz@cea.fr>
2025-12-08 11:35:37 +0100

    [PrepareRemap] RaZ out_u_lagrange + portage API Accélérateur

--- src/PrepareRemap.cc
+++ src/PrepareRemap.cc
@@ -299,74 +299,44 @@ void MahycoModule::computeVariablesForRemap_PBorn0()
       
   
   auto queue = m_acc_env->newQueue();
-  
-  // Traitement des mailles pures via les tableaux .globalVariable()
   {
-    auto command = makeCommand(queue);
-    
-    auto in_env_id           = ax::viewIn(command, m_acc_env->multiEnvMng()->envId());
-    auto in_pseudo_viscosity = ax::viewIn(command, m_pseudo_viscosity.globalVariable());
-    auto in_density          = ax::viewIn(command, m_density.globalVariable()); 
-    auto in_cell_volume      = ax::viewIn(command, m_cell_volume.globalVariable());
-    auto in_internal_energy  = ax::viewIn(command, m_internal_energy.globalVariable());
-    
-    auto out_u_lagrange = ax::viewOut(command, m_u_lagrange);
-    
-    command << RUNCOMMAND_ENUMERATE(Cell,cid,allCells()) {
-      for (Integer ivar = 0; ivar < nb_vars_to_project; ivar++) {
-        out_u_lagrange[cid][ivar] = 0;
-      }
-      Integer env_id = in_env_id[cid]; // id de l'env si maille pure, <0 sinon
-      if (env_id>=0) { // vrai ssi cid maille pure
-        // volumes matériels (partiels)
-        out_u_lagrange[cid][env_id] = in_cell_volume[cid];
-        // masses matériels (partiels)
-        out_u_lagrange[cid][nb_total_env + env_id] = in_cell_volume[cid] * in_density[cid];
-        // energies matériels (partiels)
-        out_u_lagrange[cid][2 * nb_total_env + env_id] = in_cell_volume[cid] * in_density[cid] * in_internal_energy[cid];
-        // Quantites de mouvement centrees
-        out_u_lagrange[cid][3 * nb_total_env + 0] = 0.;
-        out_u_lagrange[cid][3 * nb_total_env + 1] = 0.;
-        out_u_lagrange[cid][3 * nb_total_env + 2] = 0.;
-        // energie cinetique centree
-        out_u_lagrange[cid][3 * nb_total_env + 3] = 0.;
-        // Pseudo partiel pour la quantité de mouvement
-        out_u_lagrange[cid][3 * nb_total_env + 4] = in_cell_volume[cid] * in_pseudo_viscosity[cid];
-      }
-    }; // non-bloquant
-  }
-  
-  // Traitement des mailles mixtes
-  {
-    Integer index_env_gpu = 0;
+    {
+      auto command = makeCommand(queue);
+      auto out_u_lagrange   = ax::viewOut(command, m_u_lagrange);
+      command << RUNCOMMAND_ENUMERATE(Cell,cid,allCells()) {
+        for (Integer ivar = 0; ivar < nb_vars_to_project; ivar++) {
+          out_u_lagrange[cid][ivar] = 0;
+	}
+      };
+    }
+
     ENUMERATE_ENV(ienv,mm) {
       IMeshEnvironment* env = *ienv;
       
       // Les kernels sont lancés environnement par environnement les uns après les autres
       auto command = makeCommand(queue);
       
-      Span<const Integer> in_global_cell      (envView(m_acc_env->multiEnvMng()->globalCell(), env));
-      Span<const Real>    in_pseudo_viscosity (envView(m_pseudo_viscosity, env));
-      Span<const Real>    in_cell_volume      (envView(m_cell_volume, env));
-      Span<const Real>    in_density          (envView(m_density, env)); 
-      Span<const Real>    in_internal_energy  (envView(m_internal_energy, env));
+      auto in_pseudo_viscosity = ax::viewIn(command, m_pseudo_viscosity);
+      auto in_cell_volume      = ax::viewIn(command, m_cell_volume);
+      auto in_density          = ax::viewIn(command, m_density); 
+      auto in_internal_energy  = ax::viewIn(command, m_internal_energy);
       
       auto out_u_lagrange   = ax::viewOut(command, m_u_lagrange);
       
-      // Pour les mailles impures (mixtes), liste des indices valides 
-      Span<const Int32> in_imp_idx(env->impureEnvItems().valueIndexes());
-      Integer nb_imp = in_imp_idx.size();
-      
-      command << RUNCOMMAND_LOOP1(iter, nb_imp) {
-	auto imix = in_imp_idx[iter()[0]]; // iter()[0] \in [0,nb_imp[
-        CellLocalId cid(in_global_cell[imix]); // on récupère l'identifiant de la maille globale
+      EnvCellVectorView envcells = env->envView();
+      command << RUNCOMMAND_MAT_ENUMERATE(EnvAndGlobalCell, evgi, envcells) {
+	auto [evi,cid] = evgi();
+	Int32 index = evgi.index();
+        EnvCell ev(envcells.envCell(index));
+        Integer index_env = ev.environmentId();
+
 
         // volumes matériels (partiels)
-        out_u_lagrange[cid][index_env_gpu] = in_cell_volume[imix];
+        out_u_lagrange[cid][index_env] = in_cell_volume[evi];
         // masses matériels (partiels)
-        out_u_lagrange[cid][nb_total_env + index_env_gpu] = in_cell_volume[imix] * in_density[imix];
+        out_u_lagrange[cid][nb_total_env + index_env] = in_cell_volume[evi] * in_density[evi];
         // energies matériels (partiels)
-        out_u_lagrange[cid][2 * nb_total_env + index_env_gpu] = in_cell_volume[imix] * in_density[imix] * in_internal_energy[imix];
+        out_u_lagrange[cid][2 * nb_total_env + index_env] = in_cell_volume[evi] * in_density[evi] * in_internal_energy[evi];
         // Quantites de mouvement centrees
         out_u_lagrange[cid][3 * nb_total_env + 0] = 0.;
         out_u_lagrange[cid][3 * nb_total_env + 1] = 0.;
@@ -374,10 +344,9 @@ void MahycoModule::computeVariablesForRemap_PBorn0()
         // energie cinetique centree
         out_u_lagrange[cid][3 * nb_total_env + 3] = 0.;
         // Pseudo partiel pour la quantité de mouvement
-        out_u_lagrange[cid][3 * nb_total_env + 4] = in_cell_volume[imix] * in_pseudo_viscosity[imix];
+        out_u_lagrange[cid][3 * nb_total_env + 4] = in_cell_volume[evi] * in_pseudo_viscosity[evi];
 
       }; // bloquant
-      index_env_gpu++;
     }
   }
   
