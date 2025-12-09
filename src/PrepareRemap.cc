1987f73dea636b23a465fa1671dad55c4174cd45
Bertrand Meltz <bertrand.meltz@cea.fr>
2025-12-09 11:47:26 +0100

    [Remap] computeVariablesForRemap_PBorn0 : utilisation de menv_queue pour traiter les env indépendamments

--- src/PrepareRemap.cc
+++ src/PrepareRemap.cc
@@ -309,12 +309,15 @@ void MahycoModule::computeVariablesForRemap_PBorn0()
 	}
       };
     }
+  }
 
+  {
+    auto menv_queue = m_acc_env->multiEnvMng()->multiEnvQueue();
     ENUMERATE_ENV(ienv,mm) {
       IMeshEnvironment* env = *ienv;
       
-      // Les kernels sont lancés environnement par environnement les uns après les autres
-      auto command = makeCommand(queue);
+      // Les recopies par environnement dont indépendantes, on peut utiliser menv_queue
+      auto command = makeCommand(menv_queue->queue(env->id()));
       
       auto in_pseudo_viscosity = ax::viewIn(command, m_pseudo_viscosity);
       auto in_cell_volume      = ax::viewIn(command, m_cell_volume);
@@ -329,7 +332,6 @@ void MahycoModule::computeVariablesForRemap_PBorn0()
 	Int32 index = evgi.index();
         EnvCell ev(envcells.envCell(index));
         Integer index_env = ev.environmentId();
-
 
         // volumes matériels (partiels)
         out_u_lagrange[cid][index_env] = in_cell_volume[evi];
@@ -348,6 +350,7 @@ void MahycoModule::computeVariablesForRemap_PBorn0()
 
       }; // bloquant
     }
+    menv_queue->waitAllQueues();
   }
   
   
