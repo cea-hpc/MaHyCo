0b75b177e87e0d92d69f8e5ad2042279e3f8ba66
Bertrand Meltz <bertrand.meltz@cea.fr>
2026-02-06 09:30:09 +0100

    Scripts env et build

---
+++ env_gcc12.3_cuda12.4_mpi4.1.7.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+module purge
+#module load cmake/3.26.4 c++/gcc/12 cuda/12.4 hdf5/1.14.3 swig mpi/openmpi/4.1.7
+module load cmake/3.26.4 2 nvhpc/25.9 hdf5/1.14.3 swig mpi/openmpi/4.1.7 gcc/12.3.0
+
+export CXX=`which c++`
+export CC=`which gcc`
+export CXX CC
+
+# en attendant que SLURM_JOB_ID soit correctement configuré sur GH200
+JOB_ID="${CCCSHMDIR#/dev/shm/SLURM_}"
+export SLURM_JOB_ID=${JOB_ID}
