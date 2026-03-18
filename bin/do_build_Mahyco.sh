8b71ced7b2eee94bf0ded95f2dd7b4ba7290a141
meltzb <bertrand.meltz@protonmail.com>
2026-03-18 11:20:36 +0100

    Correction warning

--- bin/do_build_Mahyco.sh
+++ bin/do_build_Mahyco.sh
@@ -128,9 +128,7 @@ else
 fi
 
 
-ARCANE_CUDA_MODE=$(<${ARCANE_INSTALL_PATH}/${CCCOS}/${MHC_BUILD_TYPE}/lib/libarcane_accelerator_cuda_runtime.so)
-if [[ -z "${ARCANE_CUDA_MODE}" ]]
-then
+if [[ ! -f "${ARCANE_INSTALL_PATH}/${CCCOS}/${MHC_BUILD_TYPE}/lib/libarcane_accelerator_cuda_runtime.so" ]]; then
   echo "Lib libarcane_accelerator_cuda_runtime.so couldn't be found in ${ARCANE_INSTALL_PATH}. You will not be able to launch Mahyco with support of Nvidia GPUs."
   echo "Please recompile Arcane with support of Nvidia GPUs."
   exit 1
